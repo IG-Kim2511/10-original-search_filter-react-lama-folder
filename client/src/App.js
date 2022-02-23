ab19dd30b41b610bc2879696b22ae3b878d793e9
IG <iikim2511@gmail.com>
2022-02-23 18:14:21 -0500

    2nd way open

--- client/src/App.js
+++ client/src/App.js
@@ -58,61 +58,59 @@ filter, some, const keys
 3. data.filter한 것+ keys.some한 것 둘다 있으면, 
 
 includes(~) 실행
-
 */
-// function App() {
 
-//   const [query, setQuery] = useState("");
+function App() {
 
-//   const keys = ["first_name", "last_name", "email"];
+  const [query, setQuery] = useState("");
 
-//   const search = (data) => {
-//     return data.filter((item) =>
-//       keys.some((key) => item[key].toLowerCase().includes(query))
-//     );
-//   };
+  const keys = ["first_name", "last_name", "email"];
 
-// return (
-//   <div className="app">
-//       <input
-//         className="search"
-//         placeholder="Search..."
-//         onChange={(e) => setQuery(e.target.value.toLowerCase())}
-//       />
-//     {<Table data={Search(Users)} />}
-//   </div>
-// );
-// }
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
+return (
+  <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+    {<Table data={search(Users)} />}
+  </div>
+);
+}
 
 
 
 /* 🍀version 3. API SEARCH
-
-
 */
 
-function App() {
-  const [query, setQuery] = useState("");
-  const [data, setData] = useState([]);
+// function App() {
+//   const [query, setQuery] = useState("");
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(`http://localhost:5000?q=${query}`);
-      setData(res.data);
-    };
-    if (query.length === 0 || query.length > 2) fetchData();
-  }, [query]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const res = await axios.get(`http://localhost:5000?q=${query}`);
+//       setData(res.data);
+//     };
+//     if (query.length === 0 || query.length > 2) fetchData();
+//   }, [query]);
 
-  return (
-    <div className="app">
-        <input
-          className="search"
-          placeholder="Search..."
-          onChange={(e) => setQuery(e.target.value.toLowerCase())}
-        />
-      {<Table data={data} />}
-    </div>
-  );
-}
+//   return (
+//     <div className="app">
+//         <input
+//           className="search"
+//           placeholder="Search..."
+//           onChange={(e) => setQuery(e.target.value.toLowerCase())}
+//         />
+//       {<Table data={data} />}
+//     </div>
+//   );
+// }
 
 export default App;
